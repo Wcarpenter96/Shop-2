485693e9aeff9f87090fd19d767af70facfb10f0
Weston Carpenter <wc.cc96@gmail.com>
2020-01-22 15:35:14 -0800

    handled edit product errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -40,12 +40,15 @@ export const fetchProducts = () => {
 
 export const deleteProduct = productId => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://shop-55986.firebaseio.com/products/${productId}.json`,
       {
         method: "DELETE"
       }
     );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
 };
@@ -85,17 +88,23 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async dispatch => {
-    await fetch(`https://shop-55986.firebaseio.com/products/${id}.json`, {
-      method: "PATCH",
-      header: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        imageUrl
-      })
-    });
+    const response = await fetch(
+      `https://shop-55986.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        header: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
     dispatch({
       type: UPDATE_PRODUCT,
       pid: id,

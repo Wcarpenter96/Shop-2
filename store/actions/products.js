ad676ed56ac1eb4dd8f7f3f50d97ed6b36db536f
Weston Carpenter <wc.cc96@gmail.com>
2020-01-22 14:59:39 -0800

    added update and delete functionality

--- store/actions/products.js
+++ store/actions/products.js
@@ -39,7 +39,15 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async dispatch => {
+    await fetch(
+      `https://shop-55986.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE"
+      }
+    );
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -76,13 +84,26 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl
-    }
+  return async dispatch => {
+    await fetch(`https://shop-55986.firebaseio.com/products/${id}.json`, {
+      method: "PATCH",
+      header: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl
+      })
+    });
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };

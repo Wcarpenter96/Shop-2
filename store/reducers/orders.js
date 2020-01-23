5b5adceda72d5bc879a278e4e111cafbf5ee3a9e
Weston Carpenter <wc.cc96@gmail.com>
2020-01-23 15:28:30 -0800

    fetched stored orders

--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -1,5 +1,5 @@
-import { ADD_ORDER } from '../actions/orders';
-import Order from '../../models/order';
+import { ADD_ORDER, SET_ORDERS } from "../actions/orders";
+import Order from "../../models/order";
 
 const initialState = {
   orders: []
@@ -7,12 +7,16 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_ORDERS:
+      return {
+        orders: action.orders
+      };
     case ADD_ORDER:
       const newOrder = new Order(
         action.orderData.id,
         action.orderData.items,
         action.orderData.amount,
-        action.orderData.date,
+        action.orderData.date
       );
       return {
         ...state,

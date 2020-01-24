56688731cc643e79b859cdfa52620b0ae569c0a7
Weston Carpenter <wc.cc96@gmail.com>
2020-01-24 14:44:31 -0800

    managed auth load state and errors

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,7 +1,6 @@
 export const SIGNUP = "SIGNUP";
 export const LOGIN = "LOGIN";
 
-
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
@@ -19,7 +18,13 @@ export const signup = (email, password) => {
       }
     );
     if (!response.ok) {
-      throw new Error("Somethin went wrong!");
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong!";
+      if (errorId === "EMAIL_EXISTS") {
+        message = "This email already exists";
+      }
+      throw new Error(message);
     }
 
     const resData = await response.json();
@@ -30,28 +35,36 @@ export const signup = (email, password) => {
 };
 
 export const login = (email, password) => {
-    return async dispatch => {
-      const response = await fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDaZTtbuyA_g2bdWxGaRUhLp9LnTQR7rAY",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-            returnSecureToken: true
-          })
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Somethin went wrong!");
+  return async dispatch => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDaZTtbuyA_g2bdWxGaRUhLp9LnTQR7rAY",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
       }
-  
-      const resData = await response.json();
-      console.log(resData);
-  
-      dispatch({ type: LOGIN });
-    };
+    );
+    if (!response.ok) {
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong!";
+      if (errorId === "EMAIL_NOT_FOUND") {
+        message = "This email could not be found";
+      } else if (errorId === "INVALID_PASSWORD") {
+        message = "This password is not valid";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({ type: LOGIN });
   };
+};
